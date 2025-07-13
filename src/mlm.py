b2ad355cb9e5606f2685b3421755cbc7ebc736a5
Mohamed <marghoubmohamed2@gmail.com>
2025-07-13 16:21:48 +0100

    Updated the masking logic.

--- src/mlm.py
+++ src/mlm.py
@@ -1,5 +1,6 @@
 import re
 import numpy as np
+import torch
 
 def is_maskable(tokenizer, token_id, id_to_maskable):
     """Return's True if token_id is maskable."""    
@@ -15,37 +16,33 @@ def is_maskable(tokenizer, token_id, id_to_maskable):
     
     return id_to_maskable[token_id]
 
-def get_maskable_ids(tokenizer, examples, id_to_maskable):
-    """Return's the maskable indices in the examples input_ids."""
-    maskable_ids = []
+def get_maskable(tokenizer, examples, id_to_maskable):
+    """Return's a boolean mask indicating maskable positions in input_ids."""
+    maskable = []
     
     for sequence in examples["input_ids"]:
-        maskable_seq = []
-        
-        for index, token_id in enumerate(sequence):
-            if is_maskable(tokenizer, token_id, id_to_maskable):
-                maskable_seq.append(index)
-        
-        maskable_ids.append(maskable_seq)
+        maskable.append([
+            is_maskable(tokenizer, token_id, id_to_maskable) for token_id in sequence
+        ])
     
-    return maskable_ids
+    return maskable
 
 def apply_masking(tokenizer, examples, mlm_prob):
     """Randomly replace a portion of the input sequence tokens with the <mask> token."""
     input_ids = examples["input_ids"]
+    attention_mask = examples["attention_mask"]
     labels = input_ids.clone()
     
-    maskbale_ids = examples["maskable_ids"]
-    
-    for i, maskable in enumerate(maskbale_ids):
-        selected_positions = np.random.choice(
-                maskable, size = max(1, int(len(maskable) * mlm_prob))
+    maskable = examples["maskable"]
+    for i, mask in enumerate(maskable):
+        indices = torch.where(mask)[0]
+        
+        selected = np.random.choice(
+            indices, size = max(1, int(len(indices) * mlm_prob))
         )
-
-        input_ids[i, selected_positions] = tokenizer.mask_token_id
+        
+        input_ids[i, selected] = tokenizer.mask_token_id
     
     return {
-        "input_ids": input_ids,
-        "attention_mask": examples["attention_mask"],
-        "labels": labels
+        "input_ids": input_ids, "attention_mask": attention_mask, "labels": labels
     }
